1fb1980567599a9a69ea321f6af01eb6a4f86fbe
Renan Tavares <renan_velox@hotmail.com>
2021-09-30 23:43:10 -0300

    Separando a estilização da pagina home

---
+++ src/pages/home/style.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const DivInicial = styled.div`
+position: relative;
+width: 500px;
+line-height: 50px;
+text-align: center;
+margin-left: auto;
+margin-right: auto;
+top: 80px;
+
+`
+
+export const TituloInicial = styled.h1`
+text-align: center;
+font-size: 3rem;
+font-weight: lighter;
+`
