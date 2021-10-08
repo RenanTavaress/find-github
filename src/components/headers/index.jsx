ac1268ce511640ea6031484d08818c2a7f29918d
Renan Tavares <renan_velox@hotmail.com>
2021-10-08 14:55:40 -0300

    tirando codigo duplicado

--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -2,21 +2,9 @@ import { Avatar, Paper, Tab,Tabs } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router';
 import api from '../../api/api';
-import styled from 'styled-components'
-
-const Header = styled.header`
-   position: relative;
-   width: auto;
-   height: auto;
-   
-`
+import {Header, TituloHome} from './styles'
 
 
-const TituloHome = styled.h1`
-   text-align: center;
-   font-size: 3rem;
-   font-weight: lighter;
-`
 
 const Headers = ({onChange, value2}) => {
    const {user} = useParams()
